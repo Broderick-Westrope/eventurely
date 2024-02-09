95daf1022f321240f02f1907016e99e22d87cef1
Broderick Westrope <broderickwestrope@gmail.com>
2024-02-10 09:25:28 +1100

    feat: use DSN flag to connect to SQLite database

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"database/sql"
 	"log/slog"
 	"net/http"
 	"os"
 
+	_ "github.com/mattn/go-sqlite3"
 	flag "github.com/spf13/pflag"
 )
 
@@ -13,16 +15,38 @@ type application struct {
 }
 
 func main() {
-	addr := flag.String("addr", ":4000", "HTTP network address")
+	addr := flag.String("addr", ":2000", "HTTP network address")
+	dsn := flag.String("dsn", "file:test.db?cache=shared&mode=memory", "SQLite data source name")
 	flag.Parse()
 
 	app := &application{
 		logger: slog.New(slog.NewTextHandler(os.Stdout, nil)),
 	}
 
+	db, err := openDB(*dsn)
+	if err != nil {
+		app.logger.Error(err.Error())
+		os.Exit(1)
+	}
+	defer db.Close()
+
 	app.logger.Info("starting server", slog.String("addr", *addr))
 
-	err := http.ListenAndServe(*addr, app.routes())
+	err = http.ListenAndServe(*addr, app.routes())
 	app.logger.Error(err.Error())
 	os.Exit(1)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("sqlite3", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
 }
