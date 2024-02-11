8e68f95ede39b0ff43934a45efc72386474e660b
Broderick Westrope <broderickwestrope@gmail.com>
2024-02-11 21:35:08 +1100

    refactor: remove unused error helpers
    These were used in the HTTP-only implementation. Now, we need to make use of the Connect RPC error codes.

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 
 	"github.com/Broderick-Westrope/eventurely/internal/models"
 )
@@ -13,31 +12,4 @@ func validatePrivacySetting(setting models.PrivacySetting) error {
 		return fmt.Errorf("privacy setting is not valid: %v", setting)
 	}
 	return nil
-}
-
-// The serverError helper writes a log entry at Error level (including the request
-// method and URI as attributes), then sends a generic 500 Internal Server Error
-// response to the user.
-func (app *application) serverError(w http.ResponseWriter, r *http.Request, err error) {
-	var (
-		method = r.Method
-		uri    = r.URL.RequestURI()
-	)
-
-	app.logger.Error(err.Error(), "method", method, "uri", uri)
-	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-}
-
-// The clientError helper sends a specific status code and corresponding description
-// to the user. We'll use this later in the book to send responses like 400 "Bad
-// Request" when there's a problem with the request that the user sent.
-func (app *application) clientError(w http.ResponseWriter, status int) {
-	http.Error(w, http.StatusText(status), status)
-}
-
-// For consistency, we'll also implement a notFound helper. This is simply a
-// convenience wrapper around clientError which sends a 404 Not Found response to
-// the user.
-func (app *application) notFound(w http.ResponseWriter) {
-	app.clientError(w, http.StatusNotFound)
 }
